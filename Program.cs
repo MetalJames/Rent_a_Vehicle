034c020c2d7304b73e6d44f238ab9295da871b57
MetalJames <volodyaruzhak@gmail.com>
2024-03-14 19:40:13 -0400

    Start working - create Vihicle and Car Class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public abstract class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public double RentalPrice { get; set; }
+
+    public Vehicle (string model, string manufacturer, int year, double rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}");
+        Console.WriteLine($"Manufacturer: {Manufacturer}");
+        Console.WriteLine($"Year: {Year}");
+        Console.WriteLine($"Rental Price: {RentalPrice}");
+    }
+}
+
+public class Car : Vehicle
+{
+    public int Seats { get; set; }
+    public string EngineType { get; set; }
+    public string Transmission {  get; set; }
+    public bool Convertible { get; set; }
+
+    public Car (string model, string manufacturer, int year, double rentalPrice, int seats, string engineType, string transmission, bool convertible) : base(model, manufacturer, year, rentalPrice)
+    {
+        Seats = seats;
+        EngineType = engineType;
+        Transmission = transmission;
+        Convertible = convertible;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Seats: {Seats}");
+        Console.WriteLine($"EngineType: {EngineType}");
+        Console.WriteLine($"Transmission: {Transmission}");
+        Console.WriteLine($"Convertible: {(Convertible ? "Yes" : "No")}");
+    }
+}
+
+class Rent_a_Car
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("Escape", "Ford", 2010, 60, 5, "Gasoline", "Automatic", false);
+        car.DisplayDetails();
+    }
+}
