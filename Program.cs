a1521fc07f65e7f803535c228c99d8d3b583c9d1
MetalJames <volodyaruzhak@gmail.com>
2024-03-19 18:47:43 -0400

    Add truck class

--- Program.cs
+++ Program.cs
@@ -47,11 +47,50 @@ public class Car : Vehicle
     }
 }
 
+public class Truck : Vehicle
+{
+    public int Seats { get; set; }
+    public string EngineType { get; set; }
+    public string Transmission { get; set; }
+    public bool Convertible { get; set; }
+    public int Capasity { get; set; }
+    public string TruckType { get; set; }
+    public bool FourWheelDrive { get; set; }
+
+    public Truck(string model, string manufacturer, int year, double rentalPrice, int seats, string engineType, string transmission, bool convertible, 
+        int capacity, string trucktype, bool fourwheel) : base(model, manufacturer, year, rentalPrice)
+    {
+        Seats = seats;
+        EngineType = engineType;
+        Transmission = transmission;
+        Convertible = convertible;
+        Capasity = capacity;
+        TruckType = trucktype;
+        FourWheelDrive = fourwheel;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Seats: {Seats}");
+        Console.WriteLine($"EngineType: {EngineType}");
+        Console.WriteLine($"Transmission: {Transmission}");
+        Console.WriteLine($"Convertible: {(Convertible ? "Yes" : "No")}");
+        Console.WriteLine($"Capasity: {Capasity}");
+        Console.WriteLine($"TruckType: {TruckType}");
+        Console.WriteLine($"FourWheelDrive: {(FourWheelDrive ? "Yes" : "No")}");
+    }
+}
+
 class Rent_a_Car
 {
     static void Main(string[] args)
     {
         Car car = new Car("Escape", "Ford", 2010, 60, 5, "Gasoline", "Automatic", false);
         car.DisplayDetails();
+        Console.WriteLine();
+        Truck truck = new Truck("Escape", "Ford", 2010, 60, 5, "Gasoline", "Automatic", false, 8, "Freightliner", true);
+        truck.DisplayDetails();
+        Console.WriteLine();
     }
 }
